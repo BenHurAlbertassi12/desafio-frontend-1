6ef1c324a2db93c905b7fed0ae0e3085bb8436dc
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-12 18:23:37 -0300

    css inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Home nome="BenHur"></Home>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,70 @@
-export const Home = () => {
-  return <div>Home</div>;
+// MeuComponente.tsx
+import React from 'react';
+import '../style/Home.css'
+
+interface HomeComponent {
+  nome: string;
+}
+
+const Home: React.FC<HomeComponent> = ({ nome }) => {
+  return (
+    <div>
+      <div className='container'>
+        <div className='form'>
+          <h3>Confira o clima de uma cidade:</h3>
+          <div className='form-input-container'>
+            <input
+              type='text'
+              placeholder='Digite o nome da cidade'
+              id='city-input'
+            />
+            <button id='search'>
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </button>
+          </div>
+        </div>
+        <div id='weather-data' className='hide'>
+          <h2>
+            <i className='fa-solid fa-location-dot'></i> <span id='city'></span>{' '}
+            <img id='country' alt='Bandeira do país'></img>
+          </h2>
+          <p id='temperature'>
+            <span></span>&deg;C
+          </p>
+          <div id='description-container'>
+            <p id='description'></p>
+            <img id='weather-icon' src='' alt='Condições atuais' />
+          </div>
+          <div id='details-container'>
+            <p id='umidity'>
+              <i className='fa-solid fa-droplet'></i>
+              <span></span>
+            </p>
+            <p id='wind'>
+              <i className='fa-solid fa-wind'></i>
+              <span></span>
+            </p>
+          </div>
+        </div>
+        <div id='error-message' className='hide'>
+          <p>Não foi possível encontrar o clima de uma cidade com este nome.</p>
+        </div>
+        <div id='loader' className='hide'>
+          <i className='fa-solid fa-spinner'></i>
+        </div>
+        <div id='suggestions'>
+          <button id='viena'>Viena</button>
+          <button id='copenhague'>Copenhague</button>
+          <button id='zurique'>Zurique</button>
+          <button id='vancouver'>Vancouver</button>
+          <button id='genebra'>Genebra</button>
+          <button id='frankfurt'>Frankfurt</button>
+          <button id='osaka'>Osaka</button>
+          <button id='maceio'>Maceió</button>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Home;
