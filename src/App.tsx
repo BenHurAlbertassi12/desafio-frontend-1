43b9677678e6404116a58c10a8ef53f0add29bac
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-12 21:00:21 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <Home nome="BenHur"></Home>
+      <Home></Home>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,82 @@
-// MeuComponente.tsx
-import React from 'react';
-import '../style/Home.css'
+import React, { useState } from 'react';
+import '../style/Home.css';
 
-interface HomeComponent {
-  nome: string;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  sys: {
+    country: string;
+  };
+  wind: {
+    speed: number;
+  };
 }
 
-const Home: React.FC<HomeComponent> = ({ nome }) => {
+const Home: React.FC = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleLoader = () => {
+    setIsLoading((prevState) => !prevState);
+  };
+
+  const getWeatherData = async (city: string) => {
+    toggleLoader();
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    try {
+      const res = await fetch(apiWeatherURL);
+      const data = await res.json();
+
+      toggleLoader();
+
+      return data;
+    } catch (error) {
+      toggleLoader();
+      setErrorMessage('Ocorreu um erro ao obter os dados de clima.');
+      return null;
+    }
+  };
+
+  const showErrorMessage = () => {
+    setErrorMessage('Não foi possível encontrar o clima da cidade.');
+  };
+
+  const hideInformation = () => {
+    setErrorMessage('');
+    setWeatherData(null);
+  };
+
+  const showWeatherData = async (city: string) => {
+    hideInformation();
+
+    const data = await getWeatherData(city);
+
+    if (data && data.cod === '404') {
+      showErrorMessage();
+      return;
+    }
+
+    setWeatherData(data);
+  };
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    showWeatherData(city);
+  };
+
   return (
     <div>
       <div className='container'>
@@ -16,52 +86,55 @@ const Home: React.FC<HomeComponent> = ({ nome }) => {
             <input
               type='text'
               placeholder='Digite o nome da cidade'
-              id='city-input'
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
-            <button id='search'>
+            <button id='search' onClick={handleSearch}>
               <i className='fa-solid fa-magnifying-glass'></i>
             </button>
           </div>
         </div>
-        <div id='weather-data' className='hide'>
-          <h2>
-            <i className='fa-solid fa-location-dot'></i> <span id='city'></span>{' '}
-            <img id='country' alt='Bandeira do país'></img>
-          </h2>
-          <p id='temperature'>
-            <span></span>&deg;C
-          </p>
-          <div id='description-container'>
-            <p id='description'></p>
-            <img id='weather-icon' src='' alt='Condições atuais' />
+        {weatherData && weatherData.sys && (
+          <div id='weather-data'>
+            <h2>
+              <i className='fa-solid fa-location-dot'></i> {weatherData.name}{' '}
+              {weatherData.sys.country}
+            </h2>
+            <div id='weather-info-container'>
+              <div id='temperature-container'>
+                <p id='temperature'>{Math.round(weatherData.main.temp)}°C</p>
+                <p id='description'>{weatherData.weather[0].description}</p>
+              </div>
+              <div id='weather-icon-container'>
+                <img
+                  id='weather-icon'
+                  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                  alt='Condições atuais'
+                />
+              </div>
+              <div id='details-container'>
+                <p id='umidity'>
+                  <i className='fa-solid fa-droplet'></i>
+                  <span>{weatherData.main.humidity}%</span>
+                </p>
+                <p id='wind'>
+                  <i className='fa-solid fa-wind'></i>
+                  <span>{weatherData.wind.speed}km/h</span>
+                </p>
+              </div>
+            </div>
           </div>
-          <div id='details-container'>
-            <p id='umidity'>
-              <i className='fa-solid fa-droplet'></i>
-              <span></span>
-            </p>
-            <p id='wind'>
-              <i className='fa-solid fa-wind'></i>
-              <span></span>
-            </p>
+        )}
+        {errorMessage && (
+          <div id='error-message'>
+            <p>{errorMessage}</p>
           </div>
-        </div>
-        <div id='error-message' className='hide'>
-          <p>Não foi possível encontrar o clima de uma cidade com este nome.</p>
-        </div>
-        <div id='loader' className='hide'>
-          <i className='fa-solid fa-spinner'></i>
-        </div>
-        <div id='suggestions'>
-          <button id='viena'>Viena</button>
-          <button id='copenhague'>Copenhague</button>
-          <button id='zurique'>Zurique</button>
-          <button id='vancouver'>Vancouver</button>
-          <button id='genebra'>Genebra</button>
-          <button id='frankfurt'>Frankfurt</button>
-          <button id='osaka'>Osaka</button>
-          <button id='maceio'>Maceió</button>
-        </div>
+        )}
+        {isLoading && (
+          <div id='loader'>
+            <i className='fa-solid fa-spinner'></i>
+          </div>
+        )}
       </div>
     </div>
   );
